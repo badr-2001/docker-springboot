7502d05bab3a75dbd7215d7f1f4b63218b8be9db
badr <badr@ubuntu20.04.myguest.virtualbox.org>
2025-05-25 16:30:53 +0200

    add remote backend configs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -53,3 +53,42 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public.id
 }
+
+# 1. Allocate EIP
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+# 2. Create NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "main-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# 3. Create route table for private subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# 4. Associate private route table with private subnet
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
+}
